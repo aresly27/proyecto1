29150258b0d393f47186321fb6fe0c59b1b6ab6c
aresly <ilianamacias67@gmail.com>
2024-06-03 20:50:34 -0500

    Avance login

--- login/views.py
+++ login/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import login, logout
 
 # Create your views here.
+
+def ingresar (request):
+    if request.method == 'GET':
+        return render(request, 'signin.html',{
+        'form': AuthenticationForm
+        }) 
+        
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:                
+                user = User.objects.create_user(username=request.POST['username'], 
+                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except:
+                return render(request, 'registrarusuario.html',{
+                'form': UserCreationForm,
+                'error': 'Usuario ya existe'
+                })
+        return render(request, 'registrarusuario.html',{
+                'form': UserCreationForm,
+                'error': 'Contraseñas no coinciden'
+                })        
+        
+    return render(request, 'signin.html',{
+        'form': AuthenticationForm
+    })
+
